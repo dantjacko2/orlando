a5293f7b0576cc9306b3b0d034aa40c3c6df8121
dantjacko2 <dantjackson2@gmail.com>
2026-08-09 15:09:54 +0100

    Refactor familyBookings structure for clarity

--- data.js
+++ data.js
@@ -36,163 +36,216 @@ export const schedule = [
 export const familyBookings = {
 
   "2026-08-14": {
-    p: [],
-    s: [
-      "Flight 9:15am",
-      "Arrive 1:45pm",
-      "Walmart / Target",
-      "Chili's / Cheesecake Factory"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:["Flight 9:15am"],
+      a:["Arrive 1:45pm","Walmart / Target"],
+      e:["Chili's / Cheesecake Factory"]
+    }
   },
 
   "2026-08-15": {
-    p: [],
-    s: [
-      "Big Thunder 8:50am",
-      "Buzz 9:35am",
-      "Pirates 10:15am",
-      "Crystal Palace 11:30am",
-      "TRON 1:00pm"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:[
+        "Big Thunder 8:50am",
+        "Buzz 9:35am",
+        "Pirates 10:15am",
+        "Crystal Palace 11:30am"
+      ],
+      a:[
+        "TRON 1:00pm"
+      ],
+      e:[]
+    }
   },
 
   "2026-08-16": {
-    p: [],
-    s: [
-      "Teppan Edo 5:10pm",
-      "Guardians 7:05pm"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:[],
+      a:["Teppan Edo 5:10pm"],
+      e:["Guardians 7:05pm"]
+    }
   },
 
   "2026-08-17": {
-    p: [],
-    s: [
-      "Hollywood & Vine 4:50pm",
-      "Fantasmic"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:[],
+      a:["Hollywood & Vine 4:50pm"],
+      e:["Fantasmic"]
+    }
   },
 
   "2026-08-18": {
-    p: [],
-    s: [
-      "Disney Springs / Outlets"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:[],
+      a:[],
+      e:["Disney Springs / Outlets"]
+    }
   },
 
   "2026-08-19": {
-    p: [],
-    s: [
-      "Ohana 6:50pm",
-      "Fireworks from Polynesian"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:[],
+      a:["Ohana 6:50pm"],
+      e:["Fireworks from Polynesian"]
+    }
   },
 
   "2026-08-20": {
-    p: [],
-    s: [
-      "Slinky 9:15am",
-      "Toy Story Mania 10:15am",
-      "Tower of Terror 11:15am",
-      "Roundup Rodeo 5:30pm"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:[
+        "Slinky 9:15am",
+        "Toy Story Mania 10:15am",
+        "Tower of Terror 11:15am"
+      ],
+      a:[],
+      e:[
+        "Roundup Rodeo 5:30pm"
+      ]
+    }
   },
 
   "2026-08-21": {
-    p: [],
-    s: [
-      "Steakhouse 71 5:00pm"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:[],
+      a:[],
+      e:[
+        "Steakhouse 71 5:00pm"
+      ]
+    }
   },
 
   "2026-08-22": {
-    p: [
-      "Homecomin' 6:15pm"
-    ],
-    s: [
-      "Chef Art Smith's 6:15pm"
-    ]
+    p:{
+      m:[],
+      a:[],
+      e:["Homecomin' 6:15pm"]
+    },
+    s:{
+      m:[],
+      a:[],
+      e:["Chef Art Smith's 6:15pm"]
+    }
   },
 
   "2026-08-23": {
-    p: [],
-    s: [
-      "Boma 5:30pm"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:[],
+      a:[],
+      e:["Boma 5:30pm"]
+    }
   },
 
   "2026-08-24": {
-    p: [
-      "Beak & Barrel 4:20pm"
-    ],
-    s: [
-      "Topolino's 9:00am"
-    ]
+    p:{
+      m:[],
+      a:[],
+      e:["Beak & Barrel 4:20pm"]
+    },
+    s:{
+      m:["Topolino's 9:00am"],
+      a:[],
+      e:[]
+    }
   },
 
   "2026-08-25": {
-    p: [],
-    s: [
-      "Be Our Guest 5:30pm"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:[],
+      a:[],
+      e:["Be Our Guest 5:30pm"]
+    }
   },
 
   "2026-08-26": {
-    p: [
-      "Leo's first haircut",
-      "Wailulu Grill"
-    ],
-    s: [
-      "Slinky 9:00am",
-      "Toy Story Mania 9:45am",
-      "Tower of Terror 10:45am",
-      "Cinderella's Royal Table 7:00pm",
-      "Happily Ever After"
-    ]
+    p:{
+      m:["Leo's first haircut"],
+      a:[],
+      e:["Wailulu Grill"]
+    },
+    s:{
+      m:[
+        "Slinky 9:00am",
+        "Toy Story Mania 9:45am",
+        "Tower of Terror 10:45am"
+      ],
+      a:[],
+      e:[
+        "Cinderella's Royal Table 7:00pm",
+        "Happily Ever After"
+      ]
+    }
   },
 
   "2026-08-27": {
-    p: [],
-    s: [
-      "Food & Wine",
-      "Whispering Canyon 6:00pm"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:[],
+      a:["Food & Wine"],
+      e:["Whispering Canyon 6:00pm"]
+    }
   },
 
   "2026-08-28": {
-    p: [],
-    s: [
-      "Cruise embarkation"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:["Cruise embarkation"],
+      a:[],
+      e:[]
+    }
   },
 
   "2026-08-29": {
-    p: [
-      "Yak & Yeti"
-    ],
-    s: [
-      "Royal Gathering 9:45am"
-    ]
+    p:{
+      m:[],
+      a:["Yak & Yeti"],
+      e:[]
+    },
+    s:{
+      m:["Royal Gathering 9:45am"],
+      a:[],
+      e:[]
+    }
+  },
+
+  "2026-08-30": {
+    p:{m:[],a:[],e:[]},
+    s:{m:[],a:[],e:[]}
   },
 
   "2026-08-31": {
-    p: [
-      "Keke's / First Watch"
-    ],
-    s: [
-      "Disembark",
-      "Hollywood Studios → EPCOT",
-      "Eat to the Beat"
-    ]
+    p:{
+      m:["Keke's / First Watch"],
+      a:[],
+      e:[]
+    },
+    s:{
+      m:["Disembark"],
+      a:["Hollywood Studios → EPCOT"],
+      e:["Eat to the Beat"]
+    }
   },
 
   "2026-09-01": {
-    p: [],
-    s: [
-      "Flight 8:30pm"
-    ]
+    p:{m:[],a:[],e:[]},
+    s:{
+      m:[],
+      a:[],
+      e:["Flight 8:30pm"]
+    }
   }
 
 };
+    
 export const explicitOverlaps = {
  "2026-08-21":["m"], "2026-08-22":["m","e"], "2026-08-24":["m"], "2026-08-25":["a"], "2026-08-31":["a","e"]
 };
